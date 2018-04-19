f98eaf8955abff4e09a0043ae89ae55370b1161a
WilliamCrockett <william@sailing.co.za>
2018-04-19 11:42:35 -0400

    init

--- config/environment.js
+++ config/environment.js
@@ -50,7 +50,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/coach'
+    ENV.rootURL = '/'
     ENV.locationType = 'hash'
     ENV.apiHost = 'https://coach-api.herokuapp.com/'
   }
